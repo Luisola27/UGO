a7543793b061beb1157c4b3821905e5e65a4b4dd
lkiller <lui.sola27@gmail.com>
2024-04-11 22:11:57 -0600

    connection

--- ugoAPI/ConsoleApp1/obj/Debug/net8.0/ConsoleApp1.AssemblyInfo.cs
+++ ugoAPI/ConsoleApp1/obj/Debug/net8.0/ConsoleApp1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0733fbc9ed044beab7f2f5e4f2ec270379c08107")]
 [assembly: System.Reflection.AssemblyProductAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- ugoAPI/DataConnection/obj/Debug/net8.0/DataConnection.AssemblyInfo.cs
+++ ugoAPI/DataConnection/obj/Debug/net8.0/DataConnection.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DataConnection")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0733fbc9ed044beab7f2f5e4f2ec270379c08107")]
 [assembly: System.Reflection.AssemblyProductAttribute("DataConnection")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DataConnection")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
